c18dc9b5580c6f968d84402e7288314493d2f45e
romamyronenko123 <eoma575@gmail.com>
2024-07-02 20:22:41 +0300

    add template for email

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -127,4 +127,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 ACCOUNT_EMAIL_VERIFICATION = "none"
 ACCOUNT_EMAIL_REQUIRED = False
 
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = "smtp.gmail.com"
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = ""
+# EMAIL_HOST_PASSWORD = ""
+# EMAIL_USE_TLS = True
+
+
+
 LOGIN_REDIRECT_URL = 'http://127.0.0.1:8000/api/'
